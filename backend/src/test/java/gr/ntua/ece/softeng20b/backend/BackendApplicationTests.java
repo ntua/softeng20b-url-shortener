a5d5b3e9ea32fe6703fd21383ea19ad478ffb8a8
Nikolaos S. Papaspyrou <nickie@softlab.ntua.gr>
2020-12-02 19:00:26 +0200

    Add Spring boot skeleton
    Automatically generated at https://start.spring.io/
    selecting:
    
    -   Spring Web
    -   Spring Data JPA
    -   MySQL Driver

---
+++ backend/src/test/java/gr/ntua/ece/softeng20b/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.ntua.ece.softeng20b.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
